20453f258477bd6d5e30ee24db4bc43bf9eeaa13
AnimeshJha97 <AnimeshJha97@repo.github.com>
2020-12-24 11:29:55 +0530

    Update script.js

--- Count Down Timer/script.js
+++ Count Down Timer/script.js
@@ -1,11 +1,10 @@
 const newYear = "1 Jan 2021";
+daysele = document.getElementById("days");
+hoursele = document.getElementById("hours");
+minele = document.getElementById("min");
+secele = document.getElementById("sec");
 
 function countDown(){
-  daysele = document.getElementById("days");
-  hoursele = document.getElementById("hours");
-  minele = document.getElementById("min");
-  secele = document.getElementById("sec");
-
   const newYearDate = new Date(newYear);
   const currentDate = new Date();
   const totseconds = (newYearDate - currentDate) / 1000;
@@ -14,11 +13,15 @@ function countDown(){
   const hours = Math.floor((totseconds / 3600) % 24);
   const days = Math.floor(totseconds / 3600 / 24);
 
-  daysele.innerHTML = days
-  hoursele.innerHTML = hours
-  minele.innerHTML = minutes
-  secele.innerHTML = seconds
+  daysele.innerHTML = formateTime(days)
+  hoursele.innerHTML = formateTime(hours)
+  minele.innerHTML = formateTime(minutes)
+  secele.innerHTML = formateTime(seconds)
 
+}
+
+function formateTime(time){
+  return time < 10 ? `0${time}` : time;
 }
 
 countDown();
